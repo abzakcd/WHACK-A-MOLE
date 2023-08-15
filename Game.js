af0aaee950b0d71eb99e91f4b41f077b6b0023fc
Tomas545 <136482164+Tomas545@users.noreply.github.com>
2023-08-15 20:33:59 +0300

    bug fixes , score-- , start button bug
    הוספת השהייה של שניה לאחר הלחיצה על כפתור הסטארט. 
    הוספת תנאי עבור הורדת ניקוד במקרה של פספוס חפרפרת. 
    תיקון באג שצץ בלחיצה מרובה על כפתור הסטארט.
    הוספת אפשרות שליטה על רמת הקושי ע"י הוספת משתנה levelMin והסבת level==>> levelMax

--- Game.js
+++ Game.js
@@ -4,12 +4,14 @@ const bestBoard = document.querySelector('.best');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
 let timeUp = false;
+let timeUp2 = false;
 let score = 0;
 let best = 0;
-let level=2000;
+let levelMax=1000;
+let levelMin = 300;
 var modal = document.getElementById("myModal");
 var hole;
-
+let audio =new Audio("HIT2.wav");
 function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
@@ -26,7 +28,7 @@ function randomHole(holes){
 }
 
 function peep() {
-    const time = randomTime(200, level);
+    const time = randomTime(levelMin, levelMax);
     const hole = randomHole(holes);
     hole.classList.add('up');
     holeBonked = false; // Reset the holeBonked flag when a new hole is displayed
@@ -39,35 +41,59 @@ function peep() {
 
 let xx=0;
 
-function startGame(num) {
-    xx +=num;
-    if (xx>1) {
-        level -= 200;
+function startGame(e) {
+    xx +=e
+    if(xx!==1){
+        timeUp = true;
     }
+    if (xx===1) {
+       // levelMax -= 200;
     scoreBoard.textContent = 0;
     timeUp = false;
+    timeUp2 = false;
     score = 0;
-    peep();
-    setTimeout(() => timeUp = true, 15000)
-
+    levelMax=1200;
+    levelMin = 500;
+    setTimeout(()=>{peep()},1000);
+    //peep();
+    setTimeout(() => {timeUp = true; timeUp2 = true; xx = 0}, 30000);
+    }
 }
 
 
 let holeBonked = false;
 
 function bonk(e) {
-    if (hole && hole.classList.contains('up') && !holeBonked) {
-        const holess = hole.getAttribute('data-key');
-        console.log(holess);
-        console.log(e.key);
-        if (e.key == holess) {
+    const holess = hole.getAttribute('data-key');
+    if(hole.classList.contains('up') &&  e.key !== holess){
+        score--;
+        scoreBoard.textContent = score;
+    }
+    if (/*hole &&*/ hole.classList.contains('up') && !holeBonked && e.key === holess) {
+
+        /*console.log(holess);
+        console.log(e.key);*/
+        //if (e.key == holess) {
+
             score++;
+            //audio.play();
+            hole.classList.remove('up');
+            //Play_Music();
             scoreBoard.textContent = score;
             holeBonked = true; // Set the flag to true to indicate the hole has been bonked
+        /*if(score >= 10 && score % 10 === 0){
+            levelMax-=100;
+            levelMin -= 50;
+            timeUp = true;
+            if(timeUp2 !== true){
+                setTimeout(() => {timeUp = false ; peep()} , 3000);
+            }*/
+
         }
+
         if (best < score) {
             best = score;
             bestBoard.textContent = best;
         }
-    }
+
 }
