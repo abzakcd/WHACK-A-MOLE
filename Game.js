73f2bf983e06ad0a64e93b7268a121d0f88be1e7
Tomas545 <136482164+Tomas545@users.noreply.github.com>
2023-08-22 21:11:06 +0300

    bug fixes && gameplay flow
    hide of start button in game  , audio for hit && miss. bug fixes.

--- Game.js
+++ Game.js
@@ -39,61 +39,87 @@ function peep() {
     }, time);
 }
 
-let xx=0;
+//let xx = 0;
 
-function startGame(e) {
-    xx +=e
-    if(xx!==1){
-        timeUp = true;
-    }
+function startGame() {
+    /*xx = e;
     if (xx===1) {
-       // levelMax -= 200;
-    scoreBoard.textContent = 0;
-    timeUp = false;
-    timeUp2 = false;
-    score = 0;
-    levelMax=1200;
-    levelMin = 500;
-    setTimeout(()=>{peep()},1000);
-    //peep();
-    setTimeout(() => {timeUp = true; timeUp2 = true; xx = 0}, 30000);
-    }
-}
+        timeUp = true;
+        score = 0;
+        scoreBoard.textContent = score;
+        setTimeout(xx = 0, 2000);
+    }*/
+    document.getElementById('button').style.display = "none";
+   // if (xx===0) {
+        // levelMax -= 200;
+        scoreBoard.textContent = 0;
+        timeUp = false;
+        //timeUp2 = false;
+        score = 0;
+        levelMax = 1200;
+        levelMin = 500;
+        setTimeout(() => {
+            peep()
+        }, 2000);
+        //peep();
+        setTimeout(() => {
+            timeUp = true; /*timeUp2 = true;*/
+            xx = 0;
+            bestScore();
+            document.getElementById('button').style.display = "inline-block";
 
+        }, 30000);
+        //}
+  //  }
+}
 
 let holeBonked = false;
 
 function bonk(e) {
-    const holess = hole.getAttribute('data-key');
-    if(hole.classList.contains('up') &&  e.key !== holess){
-        score--;
-        scoreBoard.textContent = score;
-    }
-    if (/*hole &&*/ hole.classList.contains('up') && !holeBonked && e.key === holess) {
+    if(timeUp === false) {
+        const holess = hole.getAttribute('data-key');
+        if (/*hole.classList.contains('up') &&*/  e.key !== holess && (e.key === 'q' || e.key === 'w' || e.key === 'e' || e.key === 'r')) {
+            score -= 10;
+            scoreBoard.textContent = score;
+            playMiss();
+        }
+        if (/*hole &&*/ hole.classList.contains('up') && !holeBonked && e.key === holess) {
 
-        /*console.log(holess);
-        console.log(e.key);*/
-        //if (e.key == holess) {
+            /*console.log(holess);
+            console.log(e.key);*/
+            //if (e.key == holess) {
 
-            score++;
+            score += 10;
+            playSound();
             //audio.play();
             hole.classList.remove('up');
             //Play_Music();
             scoreBoard.textContent = score;
             holeBonked = true; // Set the flag to true to indicate the hole has been bonked
-        /*if(score >= 10 && score % 10 === 0){
-            levelMax-=100;
-            levelMin -= 50;
-            timeUp = true;
-            if(timeUp2 !== true){
-                setTimeout(() => {timeUp = false ; peep()} , 3000);
-            }*/
+            /*if(score >= 10 && score % 10 === 0){
+                levelMax-=100;
+                levelMin -= 50;
+                timeUp = true;
+                if(timeUp2 !== true){
+                    setTimeout(() => {timeUp = false ; peep()} , 3000);
+                }*/
 
         }
-
-        if (best < score) {
-            best = score;
-            bestBoard.textContent = best;
-        }
-
+    }
+}
+function playSound(){
+    const audio = new Audio("HIT2.wav");
+    audio.currentTime = 0;
+    audio.play();
+}
+function playMiss(){
+    const audio2 = new Audio("Miss.wav");
+    audio2.currentTime = 0;
+    audio2.play();
+}
+function bestScore(){
+    if (best < score) {
+        best = score;
+        bestBoard.textContent = best;
+    }
 }
